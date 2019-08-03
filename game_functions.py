ddf39f28661f6c353ce7f9616c7de1f9c28cd7b6
timliuGH <timsgithub@gmail.com>
2019-08-03 08:26:43 -0700

    Refactor check_events

--- game_functions.py
+++ game_functions.py
@@ -4,21 +4,29 @@ import sys
 
 import pygame
 
+def keydown_event(ship, event):
+    """Respond to keypress."""
+    if event.key == pygame.K_UP:
+        ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+
+def keyup_event(ship, event):
+    if event.key == pygame.K_UP:
+        ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
+
+
 def check_events(ship):
     """Respond to user events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                ship.moving_up = True
-            elif event.key == pygame.K_DOWN:
-                ship.moving_down = True
+            keydown_event(ship, event)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                ship.moving_up = False
-            elif event.key == pygame.K_DOWN:
-                ship.moving_down = False
+            keyup_event(ship, event)
 
 def update_screen(screen, settings, ship):
     """Redraw and make visible newest screen."""
